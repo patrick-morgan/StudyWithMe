3ae10406ebb6327d1283eede41562209c368fd9b
pmorg <pmorg@umich.edu>
2021-09-21 13:46:27 -0400

    Initial Commit

---
+++ StudyWithMe/StudyWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyWithMeApp.swift
+//  StudyWithMe
+//
+//  Created by Patrick Morgan on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct StudyWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
